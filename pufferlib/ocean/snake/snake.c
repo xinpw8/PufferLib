8347e10eda21a6187c8dbbf4c2d83ac3d1afb928
Joseph Suarez <jsuarez@puffer.ai>
2024-11-19 22:00:14 +0000

    Snake baseline

--- pufferlib/ocean/snake/snake.c
+++ pufferlib/ocean/snake/snake.c
@@ -3,11 +3,11 @@
 
 int main() {
     CSnake env = {
-        .num_snakes = 64,
-        .width = 135,
-        .height = 90,
+        .num_snakes = 1024,
+        .width = 1280,
+        .height = 720,
         .max_snake_length = 200,
-        .food = 256,
+        .food = 16384,
         .vision = 5,
         .leave_corpse_on_death = true,
         .reward_food = 1.0f,
@@ -15,18 +15,12 @@ int main() {
         .reward_death = -1.0f,
     };
     allocate_csnake(&env);
-
-    int batch_size = env.num_snakes;
-    int input_dim = 11*11;
-    int output_dim = 4;
-
-    //Weights* weights = load_weights("resources/snake_weights.bin", 133764);
-    //LinearLSTM* net = make_linearlstm(weights, batch_size, input_dim, output_dim);
-
-    int render_cell_size = 8;
-    Client* client = make_client(render_cell_size, env.width, env.height);
-
     reset(&env);
+
+    Weights* weights = load_weights("resources/snake_weights.bin", 148357);
+    LinearLSTM* net = make_linearlstm(weights, env.num_snakes, env.obs_size, 4);
+    Client* client = make_client(1, env.width, env.height);
+
     while (!WindowShouldClose()) {
         // User can take control of the first snake
         if (IsKeyDown(KEY_LEFT_SHIFT)) {
@@ -35,18 +29,16 @@ int main() {
             if (IsKeyDown(KEY_LEFT)  || IsKeyDown(KEY_A)) env.actions[0] = 2;
             if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) env.actions[0] = 3;
         } else {
-            /*
-            for (int i = 0; i < batch_size*input_dim; i++) {
+            for (int i = 0; i < env.num_snakes*env.obs_size; i++) {
                 net->obs[i] = (float)env.observations[i];
             }
             forward_linearlstm(net, net->obs, env.actions);
-            */
         }
         step(&env);
         render(client, &env);
     }
-    //free_linearlstm(net);
-    //free(weights);
+    free_linearlstm(net);
+    free(weights);
     close_client(client);
     free_csnake(&env);
     return 0;
