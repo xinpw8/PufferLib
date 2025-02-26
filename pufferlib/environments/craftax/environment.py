9527dab68e664da67281c6d333d834e9cb27fdcd
Joseph Suarez <jsuarez@puffer.ai>
2025-02-26 23:00:25 +0000

    Craftax binding

--- pufferlib/environments/craftax/environment.py
+++ pufferlib/environments/craftax/environment.py
@@ -6,7 +6,7 @@ import pufferlib.emulation
 def env_creator(name='Craftax-Symbolic-v1'):
     return functools.partial(make, name)
 
-def make(name, num_envs=512, buf=None):
+def make(name, num_envs=2048, buf=None):
     from craftax.craftax_env import make_craftax_env_from_name
     env = make_craftax_env_from_name(name, auto_reset=True)
     env_params = env.default_params
