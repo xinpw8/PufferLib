08ba03cc590622f930d9b10b9a54f2ea12666747
Joseph Suarez <jsuarez@puffer.ai>
2025-05-17 21:46:01 +0000

    Cam position

--- pufferlib/ocean/terraform/terraform.h
+++ pufferlib/ocean/terraform/terraform.h
@@ -467,7 +467,7 @@ Client* make_client(Terraform* env) {
     InitWindow(1080, 720, "PufferLib Terraform");
     SetTargetFPS(30);
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ 634.0f, 600.0f, 719.0f }; // Camera position
+    camera.position = (Vector3){ 720.0f, 256.0f, 720.0f }; // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
