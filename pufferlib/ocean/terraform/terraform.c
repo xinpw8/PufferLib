496a546032876acd5c66003f73424a6ed4c1b0a5
Joseph Suarez <jsuarez@puffer.ai>
2025-05-17 15:04:53 +0000

    mesh

--- pufferlib/ocean/terraform/terraform.c
+++ pufferlib/ocean/terraform/terraform.c
@@ -62,7 +62,7 @@ void demo() {
     //Weights* weights = load_weights("resources/pong_weights.bin", 133764);
     //LinearLSTM* net = make_linearlstm(weights, 1, 8, 3);
 
-    Terraform env = {.size = 11};
+    Terraform env = {.size = 128};
     allocate(&env);
 
     Client* client = make_client(&env);
--- pufferlib/ocean/terraform/terraform.h
+++ pufferlib/ocean/terraform/terraform.h
@@ -129,7 +129,7 @@ void c_reset(Terraform* env) {
     memset(env->observations, 0, env->size*env->size*sizeof(unsigned char));
     env->tick = 0;
 
-    perlin_noise(env->map, env->size, env->size, 1.0/4.0, 2, 0, 0);
+    perlin_noise(env->map, env->size, env->size, 1.0/64.0, 2, 0, 0);
 
     for (int i = 0; i < env->num_agents; i++) {
         env->dozers[i] = (Dozer){0};
@@ -167,6 +167,130 @@ void c_step(Terraform* env) {
 void c_close(Terraform* env) {
 }
 
+
+Mesh mesh_from_heightmap(float* heightMap, Vector3 size) {
+    Mesh mesh = { 0 };
+
+    int mapX = size.x;
+    int mapZ = size.z;
+
+    // NOTE: One vertex per pixel
+    mesh.triangleCount = (mapX - 1)*(mapZ - 1)*2;    // One quad every four pixels
+
+    mesh.vertexCount = mesh.triangleCount*3;
+
+    mesh.vertices = (float *)RL_MALLOC(mesh.vertexCount*3*sizeof(float));
+    mesh.normals = (float *)RL_MALLOC(mesh.vertexCount*3*sizeof(float));
+    mesh.texcoords = (float *)RL_MALLOC(mesh.vertexCount*2*sizeof(float));
+    mesh.colors = NULL;
+
+    int vCounter = 0;       // Used to count vertices float by float
+    int tcCounter = 0;      // Used to count texcoords float by float
+    int nCounter = 0;       // Used to count normals float by float
+
+    Vector3 scaleFactor = { size.x/(mapX - 1), 32.0f, size.z/(mapZ - 1) };
+
+    Vector3 vA = { 0 };
+    Vector3 vB = { 0 };
+    Vector3 vC = { 0 };
+    Vector3 vN = { 0 };
+
+    for (int z = 0; z < mapZ-1; z++)
+    {
+        for (int x = 0; x < mapX-1; x++)
+        {
+            // Fill vertices array with data
+            //----------------------------------------------------------
+
+            // one triangle - 3 vertex
+            mesh.vertices[vCounter] = (float)x*scaleFactor.x;
+            mesh.vertices[vCounter + 1] = heightMap[x + z*mapX]*scaleFactor.y;
+            mesh.vertices[vCounter + 2] = (float)z*scaleFactor.z;
+
+            mesh.vertices[vCounter + 3] = (float)x*scaleFactor.x;
+            mesh.vertices[vCounter + 4] = heightMap[x + (z + 1)*mapX]*scaleFactor.y;
+            mesh.vertices[vCounter + 5] = (float)(z + 1)*scaleFactor.z;
+
+            mesh.vertices[vCounter + 6] = (float)(x + 1)*scaleFactor.x;
+            mesh.vertices[vCounter + 7] = heightMap[(x + 1) + z*mapX]*scaleFactor.y;
+            mesh.vertices[vCounter + 8] = (float)z*scaleFactor.z;
+
+            // Another triangle - 3 vertex
+            mesh.vertices[vCounter + 9] = mesh.vertices[vCounter + 6];
+            mesh.vertices[vCounter + 10] = mesh.vertices[vCounter + 7];
+            mesh.vertices[vCounter + 11] = mesh.vertices[vCounter + 8];
+
+            mesh.vertices[vCounter + 12] = mesh.vertices[vCounter + 3];
+            mesh.vertices[vCounter + 13] = mesh.vertices[vCounter + 4];
+            mesh.vertices[vCounter + 14] = mesh.vertices[vCounter + 5];
+
+            mesh.vertices[vCounter + 15] = (float)(x + 1)*scaleFactor.x;
+            mesh.vertices[vCounter + 16] = heightMap[(x + 1) + (z + 1)*mapX]*scaleFactor.y;
+            mesh.vertices[vCounter + 17] = (float)(z + 1)*scaleFactor.z;
+            vCounter += 18;     // 6 vertex, 18 floats
+
+            // Fill texcoords array with data
+            //--------------------------------------------------------------
+            mesh.texcoords[tcCounter] = (float)x/(mapX - 1);
+            mesh.texcoords[tcCounter + 1] = (float)z/(mapZ - 1);
+
+            mesh.texcoords[tcCounter + 2] = (float)x/(mapX - 1);
+            mesh.texcoords[tcCounter + 3] = (float)(z + 1)/(mapZ - 1);
+
+            mesh.texcoords[tcCounter + 4] = (float)(x + 1)/(mapX - 1);
+            mesh.texcoords[tcCounter + 5] = (float)z/(mapZ - 1);
+
+            mesh.texcoords[tcCounter + 6] = mesh.texcoords[tcCounter + 4];
+            mesh.texcoords[tcCounter + 7] = mesh.texcoords[tcCounter + 5];
+
+            mesh.texcoords[tcCounter + 8] = mesh.texcoords[tcCounter + 2];
+            mesh.texcoords[tcCounter + 9] = mesh.texcoords[tcCounter + 3];
+
+            mesh.texcoords[tcCounter + 10] = (float)(x + 1)/(mapX - 1);
+            mesh.texcoords[tcCounter + 11] = (float)(z + 1)/(mapZ - 1);
+            tcCounter += 12;    // 6 texcoords, 12 floats
+
+            // Fill normals array with data
+            //--------------------------------------------------------------
+            for (int i = 0; i < 18; i += 9)
+            {
+                vA.x = mesh.vertices[nCounter + i];
+                vA.y = mesh.vertices[nCounter + i + 1];
+                vA.z = mesh.vertices[nCounter + i + 2];
+
+                vB.x = mesh.vertices[nCounter + i + 3];
+                vB.y = mesh.vertices[nCounter + i + 4];
+                vB.z = mesh.vertices[nCounter + i + 5];
+
+                vC.x = mesh.vertices[nCounter + i + 6];
+                vC.y = mesh.vertices[nCounter + i + 7];
+                vC.z = mesh.vertices[nCounter + i + 8];
+
+                vN = Vector3Normalize(Vector3CrossProduct(Vector3Subtract(vB, vA), Vector3Subtract(vC, vA)));
+
+                mesh.normals[nCounter + i] = vN.x;
+                mesh.normals[nCounter + i + 1] = vN.y;
+                mesh.normals[nCounter + i + 2] = vN.z;
+
+                mesh.normals[nCounter + i + 3] = vN.x;
+                mesh.normals[nCounter + i + 4] = vN.y;
+                mesh.normals[nCounter + i + 5] = vN.z;
+
+                mesh.normals[nCounter + i + 6] = vN.x;
+                mesh.normals[nCounter + i + 7] = vN.y;
+                mesh.normals[nCounter + i + 8] = vN.z;
+            }
+
+            nCounter += 18;     // 6 vertex, 18 floats
+        }
+    }
+
+    // Upload vertex data to GPU (static mesh)
+    UploadMesh(&mesh, false);
+
+    return mesh;
+}
+
 const Color PUFF_RED = (Color){187, 0, 0, 255};
 const Color PUFF_CYAN = (Color){0, 187, 187, 255};
 const Color PUFF_WHITE = (Color){241, 241, 241, 241};
@@ -177,12 +301,15 @@ typedef struct Client Client;
 struct Client {
     Texture2D ball;
     Camera3D camera;
+    Mesh mesh;
+    Model model;
+    Texture2D texture;
 };
 
 Client* make_client(Terraform* env) {
     Client* client = (Client*)calloc(1, sizeof(Client));
     int px = 64*env->size;
-    InitWindow(px, px, "PufferLib Terraform");
+    InitWindow(800, 600, "PufferLib Terraform");
     SetTargetFPS(30);
     Camera3D camera = { 0 };
     camera.position = (Vector3){ 10.0f, 30.0f, 10.0f }; // Camera position
@@ -192,6 +319,13 @@ Client* make_client(Terraform* env) {
     camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
     //client->agent = LoadTexture("resources/puffers_128.png");
     client->camera = camera;
+    client->mesh = mesh_from_heightmap(env->map, (Vector3){env->size, 1, env->size});
+    client->model = LoadModelFromMesh(client->mesh);
+
+    Image checked = GenImageChecked(env->size, env->size, 1, 1, RED, GREEN);
+    client->texture = LoadTextureFromImage(checked);
+    client->model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = client->texture;
+    UnloadImage(checked);
     return client;
 }
 
@@ -213,6 +347,7 @@ void c_render(Terraform* env) {
     Color road = (Color){35, 35, 37, 255};
     ClearBackground(road);
     BeginMode3D(client->camera);
+    /*
     for(int i = 0; i < env->size*env->size; i++) {
         float height = env->map[i];
         int x = i%env->size;
@@ -220,6 +355,8 @@ void c_render(Terraform* env) {
         DrawCube((Vector3){x, height, z}, 1.0f, 1.0f, 1.0f, DARKGREEN);
         DrawCubeWires((Vector3){x, height, z}, 1.0f, 1.0f, 1.0f, MAROON);
     }
+    */
+    DrawModel(client->model, (Vector3){0, 0, 0}, 1.0f, WHITE);
     EndMode3D();
     DrawText(TextFormat("Camera x: %f", client->camera.position.x), 10, 150, 20, PUFF_WHITE);
     DrawText(TextFormat("Camera y: %f", client->camera.position.y), 10, 170, 20, PUFF_WHITE);
