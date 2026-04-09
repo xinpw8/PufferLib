03c5573527adf014e978ccbf02bf1e81a248ac27
Spencer Cheng <spenccheng@gmail.com>
2026-04-09 14:33:04 +0000

    boulder env first commit

---
+++ ocean/boulder/binding.c
@@ -0,0 +1,22 @@
+#include "boulder.h"
+#define NUM_ATNS 2
+#define ACT_SIZES {2, 8}
+#define OBS_TENSOR_T FloatTensor
+
+#define Env Boulder
+#include "vecenv.h"
+
+void my_init(Env* env, Dict* kwargs) {
+    env->num_agents = dict_get(kwargs, "num_agents")->value;
+    env->width = dict_get(kwargs, "width")->value;
+    env->height = dict_get(kwargs, "height")->value;
+    init(env);
+}
+
+void my_log(Log* log, Dict* out) {
+    dict_set(out, "perf", log->perf);
+    dict_set(out, "score", log->score);
+    dict_set(out, "episode_return", log->episode_return);
+    dict_set(out, "episode_length", log->episode_length);
+}
+
