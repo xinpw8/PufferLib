f4852d05437b0d42696171ea4638e5106fd8cfe3
doofenshirmtz <sagnikpalchaudhuri.pkt@gmail.com>
2026-04-16 21:34:51 +0530

    added bindings

--- ocean/drmario/binding.c
+++ ocean/drmario/binding.c
@@ -1,15 +1,21 @@
 #include "drmario.h"
-#define OBS_SIZE 121
-#define NUM_ATNS 1
-#define ACT_SIZES {5}
-#define OBS_TENSOR_T ByteTensor
+
+#define OBS_SIZE 133     
+#define NUM_ATNS 1       
+#define ACT_SIZES {7}     
+#define OBS_TENSOR_T FloatTensor
 
 #define Env DrMario
 #include "vecenv.h"
 
 void my_init(Env* env, Dict* kwargs) {
     env->num_agents = 1;
-    env->size = dict_get(kwargs, "size")->value;
+    env->n_rows = dict_get(kwargs, "n_rows")->value;
+    env->n_cols = dict_get(kwargs, "n_cols")->value;
+    env->n_init_viruses = dict_get(kwargs, "n_init_viruses")->value;
+    
+    // Only init the grid (vecenv handles obs/act/rew/term buffers)
+    init(env);
 }
 
 void my_log(Log* log, Dict* out) {
@@ -17,4 +23,5 @@ void my_log(Log* log, Dict* out) {
     dict_set(out, "score", log->score);
     dict_set(out, "episode_return", log->episode_return);
     dict_set(out, "episode_length", log->episode_length);
+    dict_set(out, "viruses_cleared", log->viruses_cleared);
 }
